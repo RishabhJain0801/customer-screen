1e01862e40633d4586d32da66323a3b9f73a6b49
Rishabh Jain <1032201239@mitwpu.edu.in>
2024-03-14 10:31:14 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { SearchBar } from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
+    <div className="search-bar-container">
+        <SearchBar setResults={setResults} />
+        {results && results.length > 0 && <SearchResultsList results={results} />}
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
